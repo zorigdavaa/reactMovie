1b718a9512246b78ab0d88d822b02e8f7f4b6c05
My Mac <mymac@Mys-Mac-mini.local>
2025-09-01 22:18:21 +0800

    ss

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-blue-500">Edit55dessx.tsxss to e55dit this screen.</Text>
+      <Text className="text-red-500">new text</Text>
+    </View>
+  );
+}
+
+const redStyle = {
+  box: {
+    color: "blue"
+  }
+}
