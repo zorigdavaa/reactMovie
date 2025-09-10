724ace3cc6e9ccdf837e7d9d074bb95af697c69e
My Mac <mymac@Mys-Mac-mini.local>
2025-09-11 02:41:07 +0800

    ss

--- app/others/Login.tsx
+++ app/others/Login.tsx
@@ -1,29 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("zorigoo.nexus@yahoo.com");
+  const [password, setPassword] = useState<string>("123456");
   const [loading, setLoading] = useState<boolean>(false);
   async function login() {
     const url = "https://api.blackcandy.io/v1/login";
+    setLoading(true);
+    const response = await axios.post(
+      url,
+      { Email: email, Password: password },
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    );
 
-    const body = new URLSearchParams();
-    body.append("Email", email);
-    body.append("Password", password);
-    const res = await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: body,
-    });
-    if (!res.ok) {
-      // Server returned an error status
-      console.error("Login failed with status:", res.status);
-      return;
-    }
-    const data = await res.json();
-    console.log(data);
+    const data = response.data.json();
+    const token = data.token;
+    AsyncStorage.setItem("token", token);
+    console.log("Now data");
+    console.log(token);
   }
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -42,7 +41,7 @@ const LoginScreen = () => {
       ></TextInput>
       <Button title="Login" onPress={login} />
       <Text>
-        {" "}
+
         email is {email} and password is {password}
       </Text>
     </View>
