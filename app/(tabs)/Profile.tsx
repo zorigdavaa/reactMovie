33da7ce1cd489935d2d25bebda35f7bc02e2ad57
Zorigdavaa <zorigoo.nexus@gmail.com>
2025-09-12 15:27:16 +0800

    expo doctor

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from "react-native";
 
 const profile = () => {
   return (
     <View>
       <Text>profile</Text>
     </View>
-  )
-}
+  );
+};
 
-export default profile
+export default profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
